0001009ccc226ea35965307ef862e80eb0dbd1c9
michal <michal.jagannathan@gmail.com>
2017-03-26 00:05:48 +0100

    assignment 1 colors

--- module-1/js/app.js
+++ module-1/js/app.js
@@ -11,12 +11,15 @@ function LunchCheckController($scope) {
   $scope.message = '';
 
   $scope.checkDishes = function () {
+    $scope.message_postfix = 'warn';
+
     if ($scope.dishesInput == undefined || $scope.dishesInput == '') {
       $scope.message = "Please enter data first";
     } else if (countDishes($scope.dishesInput) > MAX_DISHES) {
       $scope.message = "Too much!"
     } else {
       $scope.message = "Enjoy!"
+      $scope.message_postfix = 'info';
     }
 
   }
