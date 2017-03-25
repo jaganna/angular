9f22e766a9a7604c31414fab9176500745355fa3
michal <michal.jagannathan@gmail.com>
2017-03-26 00:11:13 +0100

    colors

--- module-1/js/app.js
+++ module-1/js/app.js
@@ -12,7 +12,6 @@ function LunchCheckController($scope) {
 
   $scope.checkDishes = function () {
     $scope.message_postfix = 'warn';
-
     if ($scope.dishesInput == undefined || $scope.dishesInput == '') {
       $scope.message = "Please enter data first";
     } else if (countDishes($scope.dishesInput) > MAX_DISHES) {
